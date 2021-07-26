0098722ad4ac9e35dc591ae006bda1c8df7168e8
Matias Dominguez Alonso <matiasdominguez3@gmail.com>
2021-07-26 14:45:37 -0300

    Add UseProfileCard component

--- src/Components/Card/UseProfileCard.jsx
+++ src/Components/Card/UseProfileCard.jsx
@@ -5,7 +5,7 @@ import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
+import Link from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
@@ -13,39 +13,60 @@ const useStyles = makeStyles({
     maxWidth: 345,
   },
   media: {
-    height: 140,
+    height: 400,
   },
 });
 
 export default function UseProfileCard() {
+  const [name, setName] = useState("");
+  const [login, setLogin] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [userHtml, setUserHtml] = useState("");
+  const [repoHtml, setRepoHtml] = useState("");
+
+  useEffect(() => {
+    fetch("https://api.github.com/repos/mdominguez56/github-commits-api")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  const setData = ({ name, owner, html_url }) => {
+    setName(name);
+    setLogin(owner.login);
+    setAvatar(owner.avatar_url);
+    setUserHtml(owner.html_url);
+    setRepoHtml(html_url);
+  };
+
   const classes = useStyles();
 
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
+    <div>
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={avatar}
+            title="Github user Avatar"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              GitHub user: {login}
+            </Typography>
+            <Typography gutterBottom variant="h5" component="h3">
+              Repository: {name}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Link href={userHtml} target="_blank" rel="noopener">
+            Github user
+          </Link>
+          <Link href={repoHtml} target="_blank" rel="noopener">
+            Repository link
+          </Link>
+        </CardActions>
+      </Card>
+    </div>
   );
 }
